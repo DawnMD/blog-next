3dbb9aca2cdc97eb801f06e2cc54660b0955bb91
Mainak Das <mainakdas104@gmail.com>
2021-07-07 14:39:49 +0530

    imporved typography, enabled tailwind jit, added devto cta

--- components/BlogDetail/index.jsx
+++ components/BlogDetail/index.jsx
@@ -6,14 +6,23 @@ import gfm from 'remark-gfm';
 const BlogDetail = ({ blog }) => {
 	const markdown = sanitizeDevToMarkdown(blog.body_markdown);
 	return (
-		<article className='py-8 mx-auto prose prose-xl dark:prose-light'>
-			<h1>{blog.title}</h1>
-			<ReactMarkdown
-				rehypePlugins={[rehypeRaw, rehypeSanitize]}
-				remarkPlugins={[gfm]}>
-				{markdown}
-			</ReactMarkdown>
-		</article>
+		<section className='flex flex-col items-center gap-8'>
+			<article className='mx-auto prose md:prose-lg lg:prose-lg xl:prose-xl dark:prose-light'>
+				<h1>{blog.title}</h1>
+				<ReactMarkdown
+					rehypePlugins={[rehypeRaw, rehypeSanitize]}
+					remarkPlugins={[gfm]}>
+					{markdown}
+				</ReactMarkdown>
+			</article>
+			<a
+				href={blog.canonical_url}
+				className='text-xl font-medium text-center text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50'>
+				React, comment and follow on
+				<br />
+				<span className='font-bold '>Dev.To</span>
+			</a>
+		</section>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultFonts = require('tailwindcss/defaultTheme');
 module.exports = {
+	mode: 'jit',
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: 'class', // or 'media' or 'class'
 	theme: {
