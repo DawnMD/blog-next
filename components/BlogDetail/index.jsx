4684519d7d516915bcfe6a954f3f63814ec9d540
Mainak Das <mainakdas104@gmail.com>
2021-07-07 16:37:33 +0530

    removed flexbox from blogslug

--- components/BlogDetail/index.jsx
+++ components/BlogDetail/index.jsx
@@ -6,8 +6,8 @@ import gfm from 'remark-gfm';
 const BlogDetail = ({ blog }) => {
 	const markdown = sanitizeDevToMarkdown(blog.body_markdown);
 	return (
-		<section className='flex flex-col items-center gap-8'>
-			<article className='mx-auto prose md:prose-lg lg:prose-lg xl:prose-xl dark:prose-light'>
+		<>
+			<article className='py-8 mx-auto prose md:prose-lg lg:prose-lg xl:prose-xl dark:prose-light'>
 				<h1>{blog.title}</h1>
 				<ReactMarkdown
 					rehypePlugins={[rehypeRaw, rehypeSanitize]}
@@ -15,14 +15,16 @@ const BlogDetail = ({ blog }) => {
 					{markdown}
 				</ReactMarkdown>
 			</article>
-			<a
-				href={blog.canonical_url}
-				className='text-xl font-medium text-center text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50'>
-				React, comment and follow on
-				<br />
-				<span className='font-bold '>Dev.To</span>
-			</a>
-		</section>
+			<div className='text-xl font-medium text-center group'>
+				<a
+					href={blog.canonical_url}
+					className='text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-50'>
+					React, comment and follow on
+					<br />
+					<span className='font-bold '>Dev.To</span>
+				</a>
+			</div>
+		</>
 	);
 };
 
