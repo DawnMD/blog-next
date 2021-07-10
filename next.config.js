f416cd067db7d5d8adb75d118fce2abd5253931f
Mainak Das <mainakdas104@gmail.com>
2021-07-11 00:26:40 +0530

    edited env variables

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
 	images: {
 		domains: ['res.cloudinary.com', 'dev-to-uploads.s3.amazonaws.com'],
 	},
+	env: {
+		EmailServiceId: process.env.EmailServiceId,
+		EmailTemplateId: process.env.EmailTemplateId,
+		EmailUserId: process.env.EmailUserId,
+	},
 };
