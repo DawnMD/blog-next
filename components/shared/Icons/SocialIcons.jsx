beda4c9622dfcbac03015d47e8f70e11a09dd7a4
Mainak Das <mainakdas104@gmail.com>
2021-07-15 14:41:10 +0530

    added link screen readers

--- components/shared/Icons/SocialIcons.jsx
+++ components/shared/Icons/SocialIcons.jsx
@@ -4,20 +4,23 @@ const links = [
 	{
 		Icon: FaDev,
 		href: 'https://dev.to/dawnind',
+		description: 'Dev.to Social',
 	},
 	{
 		Icon: FaGithub,
 		href: 'https://github.com/DawnMD',
+		description: 'Github Social',
 	},
 	{
 		Icon: FaLinkedinIn,
 		href: 'https://www.linkedin.com/in/mainak1147',
+		description: 'LinkedIn Social',
 	},
 ];
 
 const SocialIcons = () => (
 	<div className='flex justify-center mx-auto text-2xl text-gray-500 dark:text-gray-300'>
-		{links.map(({ Icon, href }, i) => (
+		{links.map(({ Icon, href, description }, i) => (
 			<a
 				key={href}
 				href={href}
@@ -26,6 +29,7 @@ const SocialIcons = () => (
 				className={`hover:text-gray-900 dark:hover:text-gray-50 ${
 					i < links.length - 1 ? 'mr-3' : ''
 				}`}>
+				<span className='sr-only'>{description}</span>
 				<Icon />
 			</a>
 		))}
