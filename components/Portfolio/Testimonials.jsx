f3bebe4f2c052df54cc6febc00dbf5b694d0ff8a
Mainak Das <mainakdas104@gmail.com>
2021-07-16 21:07:37 +0530

    added second testimonial

--- components/Portfolio/Testimonials.jsx
+++ components/Portfolio/Testimonials.jsx
@@ -1,19 +1,39 @@
 import Section from '../shared/SectionHeading';
+const data = [
+	{
+		company: 'Next Generation Technocom Private Ltd.',
+		person: 'Subrata Mallick',
+		designation: 'CEO & Founder',
+		words:
+			'During the period of his internship program with us he was found punctual, hardworking and inquisitive.',
+	},
+	{
+		company: 'APlusTopper Pvt. Ltd.',
+		person: 'Phani Ponapalli',
+		designation: 'CEO & Founder',
+		words:
+			'During his internship period with us we found him a person who is reliable and able to ensure the assigned tasks are completed in a timely manner.',
+	},
+];
 const Testimonials = (props) => {
 	return (
 		<Section title='Testimonials'>
-			<figure className='relative flex flex-col gap-2 px-6 py-3 lg:gap-4'>
-				<blockquote className='font-serif italic before:absolute before:top-0 before:left-0 before:text-6xl before:content-quote'>
-					<p className='pl-2 text-xl font-bold tracking-wide xl:text-2xl'>
-						During the period of his internship program with us he was found
-						punctual, hardworking and inquisitive.
-					</p>
-				</blockquote>
-				<figcaption>
-					— Subrata Mallick,
-					<cite> CEO & Founder, Next Generation Technocom Private Ltd.</cite>
-				</figcaption>
-			</figure>
+			{data.map(({ company, person, designation, words }) => (
+				<figure className='relative flex flex-col gap-2 px-6 py-3 lg:gap-4'>
+					<blockquote className='font-serif italic before:absolute before:top-0 before:left-0 before:text-6xl before:content-quote'>
+						<p className='pl-2 text-xl font-bold tracking-wide xl:text-2xl'>
+							{words}
+						</p>
+					</blockquote>
+					<figcaption>
+						— {person},
+						<cite>
+							{' '}
+							{designation}, {company}
+						</cite>
+					</figcaption>
+				</figure>
+			))}
 		</Section>
 	);
 };
