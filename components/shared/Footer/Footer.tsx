5fdda2a3988fd4bc0b81c813d8e7d3d6f596c7ad
Mainak Das <mainakdas104@gmail.com>
2021-12-02 23:35:09 +0530

    Redesign ✨ footer

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -1,30 +1,52 @@
-import SocialIcons from '../Icons/SocialIcons';
-
 const Footer = (): JSX.Element => {
   return (
-    <footer className='flex flex-col items-center w-full gap-2 px-8 pb-8'>
-      <div className='my-3'>
-        <SocialIcons />
+    <footer className='flex flex-col gap-6 pb-8 md:w-[42rem] md:mx-auto text-sm mw-full'>
+      <hr className='w-full mb-2 border-gray-200 border-1 dark:border-gray-800' />
+      {/* Spotify */}
+      <div className='text-sm text-gray-400'>
+        <ul className='flex flex-col gap-3'>
+          <li>
+            <a
+              href='https://github.com/DawnMD'
+              target='_blank'
+              className='tracking-wide transition dark:hover:text-gray-300 hover:text-gray-500'>
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://dev.to/dawnind'
+              target='_blank'
+              className='tracking-wide transition dark:hover:text-gray-300 hover:text-gray-500'>
+              Dev.to
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/mainak1147/'
+              target='_blank'
+              className='tracking-wide transition dark:hover:text-gray-300 hover:text-gray-500'>
+              LinkedIn
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className='text-sm text-gray-500 dark:text-gray-300'>
-        Say hello at{' '}
-        <a
-          href='mailto:mainakdas104@gmail.com?subject=Hello'
-          className='font-semibold tracking-wider hover:text-black dark:hover:text-white'>
-          mainakdas104@gmail.com
-        </a>
-      </div>
-      <div className='flex flex-col items-center text-sm text-gray-500 dark:text-gray-300'>
+      <div className='flex items-center justify-between text-xs text-gray-400'>
         <p>
-          Made with ❤️ by{' '}
-          <span className='font-bold tracking-wide'>Mainak Das</span>
+          Made with love by{' '}
+          <a
+            className='font-semibold transition dark:hover:text-gray-300 hover:text-gray-500'
+            href='mailto:mainakdas104@gmail.com?subject=Hello'>
+            Mainak Das
+          </a>
         </p>
         <p>
           Powered by{' '}
           <a
-            href='https://docs.forem.com/api/'
-            className='font-semibold hover:text-black dark:hover:text-white'>
-            Dev.to API
+            className='font-semibold transition dark:hover:text-gray-300 hover:text-gray-500'
+            href='https://dev.to'
+            target='_blank'>
+            Dev.to
           </a>
         </p>
       </div>
