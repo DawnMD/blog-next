badf09ced5a070c1484feaad086414277d9cace3
Mainak Das <mainakdas104@gmail.com>
2021-12-03 18:12:50 +0530

    Remove unnecessary ENVs

--- next.config.js
+++ next.config.js
@@ -1,28 +1,23 @@
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ['res.cloudinary.com', 'dev-to-uploads.s3.amazonaws.com'],
-	},
-	env: {
-		EmailServiceId: process.env.EmailServiceId,
-		EmailTemplateId: process.env.EmailTemplateId,
-		EmailUserId: process.env.EmailUserId,
-	},
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Strict-Transport-Security',
-						value: 'max-age=31536000; includeSubDomains; preload',
-					},
-					{
-						key: 'X-DNS-Prefetch-Control',
-						value: 'on',
-					},
-				],
-			},
-		];
-	},
+  reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com', 'dev-to-uploads.s3.amazonaws.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+        ],
+      },
+    ];
+  },
 };
