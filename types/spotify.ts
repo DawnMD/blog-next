ec0b0055f319edd7d2a1c95b606371da6e7a45e1
Mainak Das <mainakdas104@gmail.com>
2021-12-03 18:11:45 +0530

    Add spotify types

---
+++ types/spotify.ts
@@ -0,0 +1,47 @@
+type AlbumImage = {
+  height: number;
+  url: string;
+  width: number;
+};
+
+type Artist = {
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+};
+
+export interface SpotifyNowPlayingResponse {
+  is_playing: boolean;
+  item: {
+    name: string;
+    artists: Artist[];
+    album: {
+      name: string;
+      images: AlbumImage[];
+    };
+    external_urls: {
+      spotify: string;
+    };
+  };
+}
+
+export interface SpotifyToken {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  expires_in: number;
+}
+
+export interface SpotifyNowPlayingSong {
+  album: string;
+  albumImageUrl: AlbumImage;
+  artist: string;
+  isPlaying: true;
+  songUrl: string;
+  title: string;
+}
