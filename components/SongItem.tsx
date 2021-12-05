6d1205df96aa209a3d70f28190c8fb480628a63c
Mainak Das <mainakdas104@gmail.com>
2021-12-05 15:46:33 +0530

    Add song item component

---
+++ components/SongItem.tsx
@@ -0,0 +1,45 @@
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+
+interface SongItemProps {
+  songTitle: string;
+  albumTitle: string;
+  songUrl: string;
+  albumImage: { height: number; width: number; url: string };
+  artists: string;
+}
+
+const SongItem = ({
+  albumImage,
+  albumTitle,
+  songTitle,
+  artists,
+  songUrl,
+}: SongItemProps): JSX.Element => {
+  return (
+    <NextLink href={songUrl}>
+      <a className='grid grid-cols-2 gap-4 md:flex'>
+        <div className='md:w-36 md:h-36'>
+          <NextImage
+            className='rounded'
+            width={albumImage.width}
+            height={albumImage.height}
+            src={albumImage.url}
+            loading='lazy'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <div className='flex flex-col'>
+            <h4 className='text-lg font-medium md:text-xl'>{songTitle}</h4>
+            <small className='text-gray-600 dark:text-gray-400'>
+              {albumTitle}
+            </small>
+          </div>
+          <p className='text-sm'>{artists}</p>
+        </div>
+      </a>
+    </NextLink>
+  );
+};
+
+export default SongItem;
