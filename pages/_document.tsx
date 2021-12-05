dc67b739e4f7f231c011f0a110de45af0b56041b
Mainak Das <mainakdas104@gmail.com>
2021-12-05 16:32:40 +0530

    Add preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,132 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-100.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-200.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-300.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-500.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-600.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-700.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-800.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-900.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-regular.woff'
+            as='font'
+            type='font/woff'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-100.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-200.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-300.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-500.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-600.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-700.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-800.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-900.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
+            rel='preload'
+            href='/fonts/inter-v7-latin-regular.woff2'
+            as='font'
+            type='font/woff2'
+            crossOrigin='anonymous'
+          />
+          <link
             rel='apple-touch-icon'
             sizes='180x180'
             href='/static/favicons/apple-touch-icon.png'
@@ -22,6 +148,8 @@ class MyDocument extends Document {
             sizes='16x16'
             href='/static/favicons/favicon-16x16.png'
           />
+          <meta content='#111111' name='theme-color' />
+          <meta content='#ffffff' name='msapplication-TileColor' />
           <link rel='manifest' href='/static/favicons/site.webmanifest' />
         </Head>
         <body className='text-gray-900 bg-gray-100 dark:bg-gray-900 dark:text-gray-100'>
