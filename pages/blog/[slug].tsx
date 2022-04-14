c68be2921205bf6dd89b3b2fd9c89ac3f4343053
Mainak Das <mainakdas104@gmail.com>
2022-04-14 16:01:20 +0530

    Use react markdown

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -8,8 +8,7 @@ import {
 } from '../../utils/blogUtils';
 import { formatDate } from '../../utils/helpers';
 import NextImage from 'next/image';
-import { serialize } from 'next-mdx-remote/serialize';
-import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import ReactMarkdown from 'react-markdown';
 import { sanitizeDevToMarkdown } from '../../utils/markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeCodeTitles from 'rehype-code-titles';
@@ -18,7 +17,7 @@ import { Layout } from '../../components/shared/Layout';
 import { graphcmsConnection } from '../../utils/graphcmsConnection';
 
 interface PostProps {
-  markdown: MDXRemoteSerializeResult;
+  markdown: string;
   author: { name: string; imageUrl: string };
   url: string;
   published: string;
@@ -59,7 +58,11 @@ const Post: NextPage<PostProps> = ({
           <span>{pageView.toLocaleString('en')} views</span>
         </div>
         <div className='w-full mt-4 prose md:prose-lg dark:prose-invert prose-neutral prose-img:mx-auto prose-img:rounded-lg prose-a:text-cyan-600 hover:prose-a:text-cyan-500 prose-a:transition-all dark:prose-a:text-cyan-500 dark:hover:prose-a:text-cyan-400 hover:prose-a:no-underline prose-pre:border prose-pre:border-neutral-200 prose-pre:bg-neutral-100 dark:prose-pre:border-neutral-700 dark:prose-pre:bg-neutral-900 prose-code:text-neutral-800 dark:prose-code:text-neutral-200 prose-code:px-1 prose-code:py-0.5 prose-code:bg-neutral-100 dark:prose-code:bg-neutral-900'>
-          <MDXRemote {...markdown} />
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeCodeTitles, rehypePrism]}>
+            {markdown}
+          </ReactMarkdown>
           <a href={url} className='flex justify-center mt-2'>
             Read on Dev.to
           </a>
@@ -93,22 +96,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
   }
 }`);
 
-      const markdown = await serialize(
-        sanitizeDevToMarkdown(blog.body_markdown),
-        {
-          mdxOptions: {
-            remarkPlugins: [remarkGfm],
-            rehypePlugins: [rehypeCodeTitles, rehypePrism],
-          },
-        }
-      );
       return {
         props: {
           author: {
             name: portfolioPictures[0].pictureAlt,
             imageUrl: portfolioPictures[0].porfilePicture.url,
           },
-          markdown,
+          markdown: sanitizeDevToMarkdown(blog.body_markdown),
           url: blog.url,
           published: blog.published_at,
           title: blog.title,
