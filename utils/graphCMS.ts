3fc5c3d8b57548e61f43cfd401fbde57b6f93715
Mainak Das <mainakdas104@gmail.com>
2022-04-14 17:47:53 +0530

    Add token to graphcms req

--- utils/graphCMS.ts
+++ utils/graphCMS.ts
@@ -1,5 +1,9 @@
 import { GraphQLClient } from 'graphql-request';
 
-const graphCMS = new GraphQLClient(process.env.CMS_ENDPOINT as string);
+const graphCMS = new GraphQLClient(process.env.CMS_ENDPOINT as string, {
+  headers: {
+    Authorization: `Bearer ${process.env.CMS_TOKEN}`,
+  },
+});
 
 export default graphCMS;
