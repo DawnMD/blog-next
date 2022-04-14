fb5db18b914c6b0da87ea02999933f33f0ebb517
Mainak Das <mainakdas104@gmail.com>
2022-04-14 16:11:04 +0530

    Condition render

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   return (
     <ThemeProvider attribute='class' disableTransitionOnChange>
       <Component {...pageProps} />
