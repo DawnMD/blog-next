56e0750b630dbc433bbc18a2a4d360376e8e69a2
Mainak Das <mainakdas104@gmail.com>
2022-04-14 16:06:03 +0530

    Add preact

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     domains: ['media.graphassets.com'],
   },
+  webpack(config, { dev, isServer }) {
+    // ${previousConfig...}
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
