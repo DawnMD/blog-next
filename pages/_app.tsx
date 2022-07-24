84aba853ec9d2a0bd9e3f441f64bbf874f02c25e
Mainak Das <mainakdas104@gmail.com>
2022-07-24 22:16:31 +0530

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,8 @@
-import type { AppProps } from 'next/app';
-import { DefaultSeo } from 'next-seo';
-import '../styles/globals.css';
-import { ThemeProvider } from 'next-themes';
-import { AnimatePresence } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps, router }: AppProps) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
-  const url = `${window.location.origin}${router.route}`;
-
-  return (
-    <ThemeProvider attribute='class' disableTransitionOnChange>
-      <DefaultSeo
-        titleTemplate='%s | Mainak Das'
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url,
-          description: 'The personal website for Mainak Das.',
-          site_name: 'Mainak Das | mainak.vercel.app',
-          images: [],
-        }}
-        canonical={url}
-      />
-      <AnimatePresence
-        exitBeforeEnter
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}>
-        <Component {...pageProps} key={url} />
-      </AnimatePresence>
-    </ThemeProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- utils/spotify.ts
+++
@@ -1,3 +0,0 @@
-export const basicSpotifyAuthKey = Buffer.from(
-  `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
-).toString('base64');
