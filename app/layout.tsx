e93e9bbfb686d654883f1dc91ec9785751107f88
Mainak Das <mainakdas104@gmail.com>
2022-11-05 12:29:46 +0530

    Feat: Add new font, update base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
-import './globals.css'
+import './globals.css';
+import { Space_Grotesk, Inter } from '@next/font/google';
+import { Navbar } from '../components/shared/Navbar';
 
+const grotesk = Space_Grotesk({
+  weight: 'variable',
+  variable: '--font-grotesk',
+});
+
+const inter = Inter();
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en' className={`${inter.className} ${grotesk.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <head />
-      <body>{children}</body>
+      <body className='bg-black'>
+        <div className='bg-zinc-900 min-h-screen text-zinc-400'>
+          <Navbar />
+          <main className='lg:max-w-7xl lg:mx-auto px-8'>{children}</main>
+          {/* footer */}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
