64cc4eca72fcc74bf58787efefd2564dffec74ed
Mainak Das <mainakdas104@gmail.com>
2022-11-10 00:11:47 +0530

    Fix: Update blog helpers to use json

--- utils/blogHelpers.ts
+++ utils/blogHelpers.ts
@@ -4,14 +4,14 @@ import { blogSchemaType } from 'schema/blogSchema';
 
 export const writeCache = (data: any) => {
   fs.writeFileSync(
-    path.join(process.cwd(), 'data', 'devtoBlogData.ts'),
+    path.join(process.cwd(), 'data', 'devtoBlogData.json'),
     JSON.stringify(data)
   );
 };
 
 export const readCache = () => {
   const cache = fs.readFileSync(
-    path.join(process.cwd(), 'data', 'devtoBlogData.ts'),
+    path.join(process.cwd(), 'data', 'devtoBlogData.json'),
     'utf-8'
   );
   const cacheContents = JSON.parse(cache) as blogSchemaType;
