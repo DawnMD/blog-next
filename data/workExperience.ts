f23d872036d5d7b0fab822e6bf5672f68c9a2518
Mainak Das <mainakdas104@gmail.com>
2022-11-10 01:05:28 +0530

    Feat: Add work experience data

---
+++ data/workExperience.ts
@@ -0,0 +1,27 @@
+export const workExperience = [
+  {
+    company: 'Codemancers',
+    title: 'Frontend Developer',
+    duration: '2022 - Present',
+  },
+  {
+    company: 'Codemancers',
+    title: 'Frontend Developer Intern',
+    duration: '2021 - 2021',
+  },
+  {
+    company: 'CXDeployer',
+    title: 'Fullstack Developer Intern',
+    duration: '2021 - 2021',
+  },
+  {
+    company: 'APlus Topper',
+    title: 'Frontend Developer Intern',
+    duration: '2021 - 2021',
+  },
+  {
+    company: 'Next Generation Technocom Pvt. Ltd.',
+    title: 'Application Developer Intern',
+    duration: '2020 - 2021',
+  },
+];
