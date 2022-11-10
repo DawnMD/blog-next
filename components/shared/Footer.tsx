149523f263a4c8e5e5b4c52b95775c86e89827e6
Mainak Das <mainakdas104@gmail.com>
2022-11-11 01:37:28 +0530

    Feat: Add footer dummy links

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,7 +2,13 @@ import NextLink from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className='flex flex-col items-center px-4 py-8 border-t border-zinc-600'>
+    <footer className='flex flex-col items-center px-4 py-8 border-t sm:flex-row sm:justify-between border-zinc-700 sm:px-8'>
+      <div>
+        <NextLink href='#'>Github</NextLink>
+        <NextLink href='#'>Github</NextLink>
+        <NextLink href='#'>Github</NextLink>
+        <NextLink href='#'>Github</NextLink>
+      </div>
       <p className='text-xs font-light'>
         Handcrafted with love by{' '}
         <NextLink
