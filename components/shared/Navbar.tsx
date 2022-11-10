9bcd90424f4049690a5e3de5a21c4eef5c3eed23
Mainak Das <mainakdas104@gmail.com>
2022-11-11 01:37:39 +0530

    Fix: Navbar width

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -49,7 +49,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <header className='flex items-center justify-between px-4 py-8 lg:max-w-7xl lg:mx-auto'>
+      <header className='flex items-center justify-between px-4 py-8 sm:px-8 lg:px-16 lg:max-w-7xl lg:mx-auto'>
         <NextLink
           href='/'
           className='text-2xl font-semibold font-heading text-zinc-50 lg:text-4xl'>
