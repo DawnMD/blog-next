e71ea94de94464477f04ef2e9f7947a877b8ccd7
Mainak Das <mainakdas104@gmail.com>
2022-11-11 01:37:48 +0530

    Feat: Improve blog card

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -6,6 +6,7 @@ type BlogCardProps = {
   publishedAt: string;
   description: string;
   slug: string;
+  readingTime: number;
 };
 
 export const BlogCard = ({
@@ -13,19 +14,23 @@ export const BlogCard = ({
   description,
   slug,
   title,
+  readingTime,
 }: BlogCardProps) => {
   return (
     <NextLink
-      className='flex flex-col gap-4 p-4 -mx-4 lg:mx-0 hover:bg-zinc-800'
+      className='flex flex-col gap-4 p-4 -mx-4 transition-all sm:mx-0 hover:bg-zinc-800 sm:rounded-lg'
       href={slug}>
-      <time className='pl-4 text-sm font-light border-l-2 border-teal-400 text-zinc-500'>
-        {new Date(publishedAt).toLocaleDateString('en-IN', {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </time>
+      <div className='flex items-center justify-between text-sm font-light text-zinc-500'>
+        <time className='pl-4 border-l-2 border-teal-400'>
+          {new Date(publishedAt).toLocaleDateString('en-IN', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          })}
+        </time>
+        <p>{`${readingTime} mins read`}</p>
+      </div>
       <h3 className='font-semibold text-zinc-50 font-heading'>{title}</h3>
       <p>{description}</p>
       <p className='flex items-center gap-2 text-sm text-teal-400'>
