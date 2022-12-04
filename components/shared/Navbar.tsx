20f7e33050601b8f37f52c770539c531c45ae156
Mainak Das <mainakdas104@gmail.com>
2022-12-04 13:06:58 +0530

    Fix: Add blur to modal backdrop

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -106,7 +106,7 @@ export const Navbar = () => {
             leave='ease-in duration-200'
             leaveFrom='opacity-100'
             leaveTo='opacity-0'>
-            <div className='fixed inset-0 bg-black bg-opacity-25' />
+            <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm' />
           </Transition.Child>
 
           <div className='fixed inset-0 overflow-y-auto'>
