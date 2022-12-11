0e3425e1653bad75082c588436ae1a18d1b029d5
Mainak Das <mainakdas104@gmail.com>
2022-12-12 00:51:36 +0530

    Fix: Add head element

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,9 @@ export default async function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <head />
+      <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </head>
       <body className='antialiased bg-black'>
         <div className='min-h-screen bg-zinc-900 text-zinc-400 sm:max-w-3xl lg:max-w-7xl sm:mx-auto'>
           <Navbar />
