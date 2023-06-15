64dbb3cffa15385ccb3edd0d6ac9e4965c9c0e23
Mainak Das <mainakdas104@gmail.com>
2023-06-15 18:05:42 +0530

    Feat: Add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { Footer } from '@/components/shared/Footer';
 import './globals.css';
 import localFont from 'next/font/local';
+import { Header } from '@/components/shared/Header';
 
 const CalSans = localFont({
   src: '../public/fonts/CalSans/CalSans-SemiBold.woff2',
@@ -37,8 +39,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${CalSans.variable} ${Matter.variable} font-sans`}>
-        {children}
+      <body
+        className={`${CalSans.variable} ${Matter.variable} font-sans antialiased bg-black`}>
+        <div className='min-h-screen bg-zinc-900 text-zinc-400 sm:max-w-3xl lg:max-w-7xl sm:mx-auto flex flex-col'>
+          <Header />
+          <main className='lg:max-w-7xl lg:mx-auto px-4 pb-4 lg:px-16 sm:px-8 grow'>
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
