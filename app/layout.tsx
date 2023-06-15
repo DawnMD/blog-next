0ede15d778ebadeea42a5f2b3a87a9d1a5d7e63f
Mainak Das <mainakdas104@gmail.com>
2023-06-15 12:32:16 +0530

    Feat: Initial commit
    - Initialize Fonts
    - Remove defaults

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,34 @@
 import './globals.css';
+import localFont from 'next/font/local';
+
+const CalSans = localFont({
+  src: '../public/fonts/CalSans/CalSans-SemiBold.woff2',
+  display: 'swap',
+  style: 'normal',
+  weight: '600',
+  variable: '--font-calSans',
+});
+
+const Matter = localFont({
+  src: [
+    {
+      path: '../public/fonts/Matter/Matter-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Matter/Matter-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Matter/Matter-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-matter',
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +37,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={`${CalSans.variable} ${Matter.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div>NextJS Portfolio</div>;
+  return (
+    <main>
+      <h1 className='scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl font-calSans'>
+        Mainak Das
+      </h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quo
+        distinctio odit, illum, vel ipsam voluptatem laborum commodi dolor
+        soluta error, earum cumque. Vitae odit voluptates mollitia tenetur,
+        commodi eligendi?
+      </p>
+    </main>
+  );
 }
